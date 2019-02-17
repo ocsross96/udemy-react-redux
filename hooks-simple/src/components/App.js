493fd0c3705e3ccf20a1c80898bab805951ed7cf
Ross Matthews <ross.matthews@ratedpeople.com>
2019-02-17 13:37:19 +0000

    Finished first part of hooks lessons

--- hooks-simple/src/components/App.js
+++ hooks-simple/src/components/App.js
@@ -1,16 +1,18 @@
-import React, { Component, useState } from 'react';
+import React, { useState } from 'react';
+import ResourceList from './ResourceList';
+import UserList from './UserList';
 
 const App = () => {
   const [resource, setResource] = useState('posts');
-  const [currentCount, setCount] = useState(0);
 
   return (
     <div>
+      <UserList />
       <div>
         <button onClick={() => setResource('posts') }>Posts</button>
         <button onClick={() => setResource('todos') }>Todos</button>
       </div>
-      {resource}
+      <ResourceList resource={resource} />
     </div>
   );
 };
